0f3e17efd4fbb04e67c25e9f02f6b5423a20d207
Илья Баранов <ilya.baranov00@yandex.ru>
2023-11-20 21:32:00 +0300

    Добавьте файлы проекта.

---
+++ ASPApp/Controllers/WorkerController.cs
@@ -0,0 +1,35 @@
+﻿using ASPApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkerController : ControllerBase
+    {
+
+        private readonly Worker _worker;
+
+        public WorkerController(Worker worker)
+        {
+            _worker = worker;
+        }
+
+        [HttpGet("start")]
+        public async Task<IActionResult> Start()
+        {
+            var startDate = DateTime.Now;
+            await _worker.StartAsync(CancellationToken.None);
+            var endDate = DateTime.Now;
+            return Ok($"Worker worked {(endDate - startDate).TotalSeconds} sec");
+        }
+        [HttpGet("stop")]
+        public async Task<IActionResult> Stop()
+        {
+            await _worker.StopAsync(CancellationToken.None);
+            return Ok("Worker stop");
+
+        }
+    }
+}
